b30cc10966b468eac6fc058d1c5875ec11c9f2d1
Krystian Petek <krystianpetek2@gmail.com>
2023-01-12 20:40:49 +0100

    Added complaint controller with mock'ed CRUD methods

---
+++ KriniteAuthServer.ResourceDataAPI/Controllers/ComplaintController.cs
@@ -0,0 +1,42 @@
+﻿using KriniteAuthServer.ResourceDataAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KriniteAuthServer.ResourceDataAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ComplaintController
+{
+    public ComplaintController() { }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ComplaintModel>>> GetComplaintsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ComplaintModel>> GetComplaintAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddComplaintAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<ComplaintModel>> UpdateComplaintAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ComplaintModel>> RemoveComplaintAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+}
