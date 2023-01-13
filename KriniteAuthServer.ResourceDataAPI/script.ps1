a6f17728ffd048703969fce41548a4a2e05af631
Krystian Petek <krystianpetek2@gmail.com>
2023-01-13 21:03:50 +0100

    Added docker compose support for https services because created dev-certs and writed it in container environment

--- KriniteAuthServer.ResourceDataAPI/script.ps1
+++ KriniteAuthServer.ResourceDataAPI/script.ps1
@@ -1,1 +1,4 @@
 ﻿docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=NotAll0wedForPublic" -p 7821:1433 -d --name KriniteAuthServer-ResourceDataAPI mcr.microsoft.com/mssql/server:latest
+dotnet dev-certs https -ep %PATH%\dev-cert.pfx -p "DoNotTellAny0ne"
+dotnet dev-certs https --trust
+docker inspect db9c472734bfea92aa9d3d9880a990832a6e2fe0d5e52f52d38aadd2915fbdd4
