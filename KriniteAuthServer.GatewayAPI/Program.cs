265cee69ce196b5f5010a751803c073a523ce511
Krystian Petek <krystianpetek2@gmail.com>
2023-01-16 17:28:21 +0100

    Added ocelot configuration to getting response from ResponseDataAPI using GatewayAPI

--- KriniteAuthServer.GatewayAPI/Program.cs
+++ KriniteAuthServer.GatewayAPI/Program.cs
@@ -1,15 +1,22 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 namespace KriniteAuthServer.GatewayAPI;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
         builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+        builder.Services.AddOcelot();
+
         var app = builder.Build();
 
         app.MapGet("/", () => "Hello World!");
 
+        await app.UseOcelot();
         app.Run();
     }
 }
--- KriniteAuthServer.ResourceDataAppHybrid/ApiServices/ApiEndpoints.cs
+++ KriniteAuthServer.ResourceDataAppHybrid/ApiServices/ApiEndpoints.cs
@@ -3,6 +3,6 @@
 public static class ApiEndpoints
 {
     //public const string Complaints = "http://kriniteauthserver.resourcedataapi";
-    public const string Complaints = "https://localhost:7820/api/complaint";
+    public const string Complaints = "https://localhost:7825/complaint";
     public const string Authorization = "https://localhost:7822/connect/authorize";
 }
